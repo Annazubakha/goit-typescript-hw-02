995427e977535e6e21c777ce18699ec61afcb1d3
Annazubakha <korepovaanna@gmail.com>
2024-02-02 21:19:26 +0100

    add tasks

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,17 +1,22 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
-
-class Component {
-  constructor (public props:T) {
-
-  }
+interface IProps {
+  title: string;
+}
+class Component<T> {
+  constructor(public props: T) {}
 }
 
-class Page extends Component {
-  pageInfo () {
+class Page extends Component<IProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
 
 export {};
+
+// •	4 Завдання
+//          interface повинен бути один і використовоуємо interface при оголошення класу Page
+// interface IProps {}
+// class Page extends Component<IProps> {}
